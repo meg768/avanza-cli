35303eb6eef7857b863dcdfa6490b0a7f74e3e2c
meg768 <meg768@users.noreply.github.com>
2022-05-19 22:48:09 +0200

    -

--- src/commands/overview.js
+++ src/commands/overview.js
@@ -24,10 +24,19 @@ module.exports = class extends Command {
 			return width ? EasyTable.padLeft(text, width) : text;
 		};
 
+		let rightAlignPrinter = (value, width) => {
+			return EasyTable.padLeft(value, width);
+		};
+
 		array = array.filter((item) => {
 			return item.ownCapital > 1;
 		});
 
+		function percentacePrinter(value, width) {
+			value = value.toFixed(2)
+			return width ? EasyTable.padLeft(value, width) : value;
+		}
+		
 		let accountType = {
 			'KapitalforsakringBarn': 'Kapitalförsäkring (barn)',
 			'Kapitalforsakring': 'Kapitalförsäkring',
@@ -41,11 +50,14 @@ module.exports = class extends Command {
 			table.cell('Namn', item.name);
 			table.cell('Typ', accountType[item.accountType] == undefined ? '-' : accountType[item.accountType]);
 			table.cell('Konto', item.accountId);
+			table.cell('Utveckling %', item.performancePercent, percentacePrinter);
+			table.cell('Utveckling SEK', item.performance, moneyPrinter);
 			table.cell('Värde', item.ownCapital, moneyPrinter);
 			table.newRow();
 		});
 
 //		table.sort(['Konto', 'Namn']);
+		table.total('Utveckling SEK', {printer: moneyPrinter});			
 		table.total('Värde', {printer: moneyPrinter});			
 
 		this.log(table.toString());
